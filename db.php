9ef6867a0ab78164606010d2a04be9670e75609a
Your Name <you@example.com>
2021-03-11 13:48:48 +0700

    sf

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-    define("hostname","localhost");
-    define("user","root");
-    define("passwpord","");
-    define("dbname","bookstore");
+define("hostname", "localhost");
+define("user", "root");
+define("passwpord", "");
+define("dbname", "bookstore");
 ?>
 <?php
-$conn=new mysqli(hostname,user,passwpord,dbname);
-$conn->query("SELECT * FROM `book` WHERE 1");
-if($conn->connect_error) echo "not connect";
+$conn = new mysqli(hostname, user, passwpord, dbname);
+$conn->query("SELECT * FORM `book` WHERE 1");
+if ($conn->connect_error) echo "not connect";
 else echo "Connect success";
 ?>
