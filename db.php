f2a17882127b0334556262222e966764b4e8b5a3
Your Name <you@example.com>
2021-03-11 13:49:22 +0700

    update dp

--- db.php
+++ db.php
@@ -7,6 +7,6 @@ define("dbname", "bookstore");
 <?php
 $conn = new mysqli(hostname, user, passwpord, dbname);
 $conn->query("SELECT * FORM `book` WHERE 1");
-if ($conn->connect_error) echo "not connect";
+if ($conn->connect_error) echo "not connect.";
 else echo "Connect success";
 ?>
